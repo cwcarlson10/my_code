6f8616f4f3a5a47f5ba3eeff7fd1463b0279b768
Chris Carlson <chriswc10@chriscasmacbook.pk5001z>
2014-11-30 16:29:44 -0700

    this is most recent

--- calculator2.rb
+++ calculator2.rb
@@ -1,12 +1,12 @@
 
 class Calculator
 
-  while
+  
     def get_input
       puts "Welcome to Calculator!"
 
-        puts "To begin calcuation enter '1'. To quit enter '0'"
-        option = gets.chomp.to_i - 1
+      puts "To begin calcuation enter '1'. To quit enter '0'"
+      option = gets.chomp.to_i - 1
 
         if option == 0
           puts "here we go!"
@@ -14,44 +14,41 @@ class Calculator
           puts "Thanks for calculating!"
         end
 
-        puts "What is your first number?"
+      puts "What is your first number?"
 
-        @num1 = gets.chomp.to_i
-        puts "Do you want to: 1- Add, 2- Subtract, 3- Multiply, or 4- Divide #{@num1}"
-        choice = gets.chomp.to_i - 1
-          if choice > 3
-            puts "please enter 1, 2, 3, 4"
-          end
-        puts "what is your second number?"
-        @num2 = gets.chomp.to_i
-          if choice == 0
-            answer = add
-            puts " Your answer is :#{answer}"
-          elsif choice == 1
-            answer = subtract
-            puts " Your answer is :#{answer}"
-          elsif choice == 2
-            answer = multiply
-            puts " Your answer is :#{answer}"
-          elsif choice == 3
-            answer = divide
-            puts " Your answer is :#{answer}"
-          end
-
+      @num1 = gets.chomp.to_i
+      puts "Do you want to: 1- Add, 2- Subtract, 3- Multiply, or 4- Divide #{@num1}"
+      choice = gets.chomp.to_i - 1
+        if choice > 3
+          puts "please enter 1, 2, 3, 4"
+        end
+      puts "what is your second number?"
+      @num2 = gets.chomp.to_i
+        if choice == 0
+          answer = add
+          puts " Your answer is :#{answer}"
+        elsif choice == 1
+          answer = subtract
+          puts " Your answer is :#{answer}"
+        elsif choice == 2
+          answer = multiply
+          puts " Your answer is :#{answer}"
+        elsif choice == 3
+          answer = divide
+          puts " Your answer is :#{answer}"
+        end
     end
 
     def add
       @num1 + @num2
     end
-
+   
     def subtract
       @num1 - @num2
-
     end
 
     def multiply
       @num1 * @num2
-
     end
 
     def divide
@@ -59,7 +56,7 @@ class Calculator
       new_answer2 = @num2.to_f
       new_answer / new_answer2
     end
-  end
+  
 
 # add
 # subtract
--- tick_tac_toe.rb
+++ tick_tac_toe.rb
@@ -1,38 +1,143 @@
 
-
-line_one   = []
-line_two   = []
-line_three = []
-
-upper_left    = line_one[0]
-middle_left   = line_two[0]
-bottom_left   = line_three[0]
-upper_center  = line_one[1]
-middle_center = line_two[1]
-lower_center  = line_three[1]
-upper_right   = line_one[2]
-middle_right  = line_two[2]
-lower_right   = line_three[2]
-
-
-  puts "Welcome to Tic Tac Toe"
-
-
-  puts "Please place your first X by selecting 1, 2, or 3 then 4, 5, or 6"
-
-  puts "1 = top row"
-  puts "2 = middle row"
-  puts "3 = bottom row"
-  puts ""
-  puts "4 = left"
-  puts "5 = center"
-  puts "6 = right"
-
-  player_selection = gets.chomp.split(",")
-
-  if player_selection == ('1 4')
-    puts "X to top left"
+class TicTacToe
+#set up game board
+  def initialize
+    @board = []
+    line_one =   [1, 2, 3]
+    line_two =   [4, 5, 6]
+    line_three = [7, 8, 9]
+    @board << line_one
+    @board << line_two
+    @board << line_three
   end
+#display game board
+  def display
+    @board.each do |d|
+      puts d.collect {|p| p }.join(' ')
+    end
+    rules
+  end
+#set and explain the rules of the game and get first player selection
+   def new_game
+      puts 'Do you want to play again? (y/n)'
+      play_again = gets.chomp.downcase
+      if play_again == 'y'
+        a = TicTacToe.new
+        a.display
+      else play_again == 'n'
+        b = TicTacToe.new
+        b.goodbye
+      end
+    end
+      
+    def goodbye
+      puts 'Thanks for playing!'
+    end
+  
+    def rules
+      @score = true
+      while @score == true
+        puts "get 3 x's in a row to win"
+        puts "Player 1: to place an x select the number of the box you want and press enter"
+        answer = gets.chomp.to_i
+        case answer
+        when 1
+          @board[0][0] = 'x'
+        when 2
+          @board[0][1] = 'x'
+        when 3
+          @board[0][2] = 'x'
+        when 4
+          @board[1][0] = 'x'
+        when 5
+          @board[1][1] = 'x'
+        when 6
+          @board[1][2] = 'x'
+        when 7
+          @board[2][0] = 'x'
+        when 8
+          @board[2][1] = 'x'
+        when 9
+          @board[2][2] = 'x'
+        end
+        puts "Player 2 : to place and o select the number box you want and press enter"
+        player2_answer = gets.chomp.to_i
+        case player2_answer
+        when 1
+          @board[0][0] = 'o'
+        when 2
+          @board[0][1] = 'o'
+        when 3
+          @board[0][2] = 'o'
+        when 4
+          @board[1][0] = 'o'
+        when 5
+          @board[1][1] = 'o'
+        when 6
+          @board[1][2] = 'o'
+        when 7
+          @board[2][0] = 'o'
+        when 8
+          @board[2][1] = 'o'
+        when 9
+          @board[2][2] = 'o'
+        end
+
+
+        if @board[0][0] == 'x' && @board[0][1] == 'x' && @board[0][2] == 'x'
+          puts 'x wins!'
+          @score = false
+          if @score == false 
+            return false
+          end
+        elsif @board[1][0] == 'x' && @board[1][1] == 'x' && @board[1][2] == 'x'
+          puts 'x wins!'
+        elsif @board[2][0] == 'x' && @board[2][1] == 'x' && @board[2][2] == 'x'
+          puts 'x wins!'
+        elsif @board[0][0] == 'x' && @board[1][0] == 'x' && @board[2][0] == 'x'
+          puts 'x wins!'
+        elsif @board[0][1] == 'x' && @board[1][1] == 'x' && @board[2][1] == 'x'
+          puts 'x wins!'
+        elsif @board[0][2] == 'x' && @board[1][2] == 'x' && @board[2][2] == 'x'
+          puts 'x wins!'
+        elsif @board[0][0] == 'x' && @board[1][1] == 'x' && @board[2][2] == 'x'
+          puts 'x wins!'
+        elsif @board[0][2] == 'x' && @board[1][1] == 'x' && @board[2][0] == 'x'
+          puts 'x wins!'
+        elsif @board[0][0] == 'o' && @board[0][1] == 'o' && @board[0][2] == 'o'
+          puts 'o wins!'
+        elsif @board[1][0] == 'o' && @board[1][1] == 'o' && @board[1][2] == 'o'
+          puts 'o wins!'
+        elsif @board[2][0] == 'o' && @board[2][1] == 'o' && @board[2][2] == 'o'
+          puts 'o wins!'
+        elsif @board[0][0] == 'o' && @board[1][0] == 'o' && @board[2][0] == 'o'
+          puts 'o wins!'
+        elsif @board[0][1] == 'o' && @board[1][1] == 'o' && @board[2][1] == 'o'
+          puts 'o wins!'
+        elsif @board[0][2] == 'o' && @board[1][2] == 'o' && @board[2][2] == 'o'
+          puts 'o wins!'
+        elsif @board[0][0] == 'o' && @board[1][1] == 'o' && @board[2][2] == 'o'
+          puts 'o wins!'
+        elsif @board[0][2] == 'o' && @board[1][1] == 'o' && @board[2][0] == 'o'
+          puts 'o wins!'
+        end
+        puts ''
+
+        display
+        puts ''
+      end
+      new_game
+    end
+
+   
+  
+#get random choice from computer
+#ask if player wants to play again
+end
+a = TicTacToe.new
+a.display
+
+
 
 
 
